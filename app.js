500fd7210042ad810185d060eacbfa17e9fe5764
ayinfly <ayinflyschool@gmail.com>
2023-08-13 19:32:19 -0700

    add comments and get all elements as js objects

---
+++ app.js
@@ -0,0 +1,6 @@
+const allCardsBtn = document.getElementById("all-cards");
+const usersBtn = document.getElementById("users");
+const loginBtn = document.getElementById("login");
+const registerBtn = document.getElementById("register");
+const allCardsTempBtn = document.getElementById("all-cards-temp");
+const registerTempBtn = document.getElementById("register-temp");
